7e1e3e377c49b450981e4de0a2ce12f2c59479c6
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-11 22:18:04 +0300

    add Example Dog

---
+++ Example008_dog/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+double distance = 10000;                // метры
+double firstFriendSpeed = 1;            // м/с
+double secondFriendSpeed = 2;           // м/с
+double dogSpeed = 5;                    // м/с
+int friend = 2;                         // направление движения: к другу 1 или 2
+double time;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count = count + 1;
+    
+}
+Console.WriteLine(count);
