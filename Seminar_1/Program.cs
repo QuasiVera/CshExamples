463f7b0a3a65aab91e8b24f54a7aada066f00dd4
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-11 22:18:58 +0300

    add С семинара по  Си Шарп

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1. Получить строку, содержащую число
+Console.Write("Введите целое число: ");
+string userInput = Console.ReadLine() ?? ""; // проверка на null (warning S8600)
+
+// 2. Преобразовать строку в число
+int number = int.Parse(userInput);
+
+// 3. Возводим число в квадрат
+int result = number*number;
+
+// 4. Выводим результат в консоль
+Console.WriteLine($"{number} -> {result}");     // $"{number} -> {result}" интерполяция строк
