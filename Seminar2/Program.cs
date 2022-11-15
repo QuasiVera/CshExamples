d7a6b8ea1d967d21c8bb8fb0956bea51c43d30c8
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-15 22:05:28 +0300

    add Seminar 2

---
+++ Seminar2/Program.cs
@@ -0,0 +1,17 @@
+﻿// 
+int number = new Random().Next(10, 100);
+
+int a = number / 10;
+int b = number % 10;
+
+int max;
+
+if (a > b)
+    max = a;
+else 
+    max = b;
+
+Console.WriteLine($"{number} -> {max}");
+
+//double result = (double) 23/10;           -> чтобы получить дробный результат от деления
+//Console.WriteLine($"result = {result} ");
