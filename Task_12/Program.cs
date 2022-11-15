49d2253c02bc19b8c925a3daaaf0b9d00b3f1eb4
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-15 22:06:17 +0300

    add Task 12

---
+++ Task_12/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число 1: ");
+string inputNumber1 = Console.ReadLine() ?? "";
+
+Console.WriteLine("Введите число 2: ");
+string inputNumber2 = Console.ReadLine() ?? "";
+
+int number1 = int.Parse(inputNumber1);
+int number2 = int.Parse(inputNumber2);
+
+int result = number1 % number2;
+if (result==0)
+{
+    Console.WriteLine($"{number1}, {number2} -> кратно");
+}
+else
+{
+    Console.WriteLine($"{number1}, {number2} -> не кратно, остаток = {result}");
+}
+
